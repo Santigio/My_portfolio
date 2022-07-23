a4af12e21fd9ac92eb33adbb59d689365b94859e
santigio <santigiesankoh233@gmail.com>
2022-07-23 15:26:55 +0200

    my portfolio

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./header.css";
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Santigie Sankoh</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scrool__down">Scroll Down</a>
+        
+      </div>
+    </header>
+  )
+};
+
+export default Header;
